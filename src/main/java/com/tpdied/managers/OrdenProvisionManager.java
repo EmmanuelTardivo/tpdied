519cc17271de9143905d0527dc2e303155a5cf69
EmmanuelTardivo <emmanuel.tardivo@yahoo.com.ar>
2023-08-02 17:30:58 -0300

    Corregido el método que devuelve todos los caminos posibles en el manager. Agregué getPeso al OrdenDTO para que calcule el peso total de la orden

--- src/main/java/com/tpdied/managers/OrdenProvisionManager.java
+++ src/main/java/com/tpdied/managers/OrdenProvisionManager.java
@@ -2,8 +2,10 @@ package com.tpdied.managers;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -25,23 +27,61 @@ public class OrdenProvisionManager {
         sucursalController = new SucursalController(entityManager);
     }
 
-    public List<List<RutaDTO>> getCaminos(OrdenProvisionDTO orden) {
+    public Set<List<RutaDTO>> getCaminosPosibles(OrdenProvisionDTO orden) {
+        
+        Set<List<RutaDTO>> caminos = new HashSet<>();
+        SucursalDTO destino = orden.getSucursalDestino();
+        Integer limiteTiempoEnMinutos = pasarAMinutos(orden.getLimiteTiempo());
+        List<SucursalDTO> sucursalesCandidatas = getOrigenesPosibles(orden);
+        Double pesoOrden = orden.getPeso();
 
-        SucursalDTO sucursalDestino = orden.getSucursalDestino();
-        Map<ProductoDTO, Integer> productosSolicitados = orden.getItemsProductoCantidad();
-        List<SucursalDTO> sucursales = sucursalController.getSucursalesByEstado(true);
-        sucursales.remove(sucursalDestino);
-        sucursales = sucursales.stream()
-                .filter(s -> satisfaceLaOrden(s.getListaProductoCantidadEnStock(), productosSolicitados))
-                .collect(Collectors.toList());
+        for (SucursalDTO candidata : sucursalesCandidatas) {
+            caminos.addAll(getCaminos(candidata, destino, limiteTiempoEnMinutos, pesoOrden));
+        }
+        return caminos;
+
+    }
+
+    private List<List<RutaDTO>> getCaminos(SucursalDTO origen, SucursalDTO destino, int limiteTiempoEnMinutos, double pesoOrden) {
         List<List<RutaDTO>> caminos = new ArrayList<>();
-        for (SucursalDTO origen : sucursales) {
-            List<RutaDTO> caminoActual = new ArrayList<>();
-            encontrarCaminos(origen, sucursalDestino, new ArrayList<>(), caminoActual, caminos,
-                    pasarAMinutos(orden.getLimiteTiempo()));
+        List<RutaDTO> caminoActual = new ArrayList<>();
+        getCaminosDesdeOrigen(origen, destino, caminoActual, caminos, limiteTiempoEnMinutos, 0, pesoOrden);
+        return caminos;
+    }
+
+    private void getCaminosDesdeOrigen(SucursalDTO actual, SucursalDTO destino,
+            List<RutaDTO> caminoActual, List<List<RutaDTO>> caminos,
+            int limiteTiempoEnMinutos, int tiempoTranscurrido, double pesoOrden) {
+
+        if (actual.equals(destino)) { // verificar si se alcanzó el destino
+            caminos.add(new ArrayList<>(caminoActual)); // Se encontró un camino válido, se agrega a la lista de caminos
+            return;
         }
 
-        return caminos;
+        List<RutaDTO> rutasDesdeActual = rutaController.getRutasBySucursalOrigen(actual).stream()
+                .filter(r -> r.getEstado()).toList();
+
+        for (RutaDTO ruta : rutasDesdeActual) {
+            double pesoMaximoRuta = ruta.getCapacidadEnKilos();
+            int tiempoActualizado = tiempoTranscurrido + pasarAMinutos(ruta.getDuracionViaje());
+            ;
+            // Verificar si se excede el límite de tiempo antes de explorar la ruta y si el
+            // peso se puede transportar
+            if (tiempoActualizado <= limiteTiempoEnMinutos && pesoMaximoRuta >= pesoOrden) {
+                caminoActual.add(ruta);
+                getCaminosDesdeOrigen(ruta.getSucursalDestino(), destino, caminoActual, caminos,
+                        limiteTiempoEnMinutos, tiempoActualizado, pesoOrden);
+                caminoActual.remove(ruta);
+            }
+        }
+    }
+
+    private List<SucursalDTO> getOrigenesPosibles(OrdenProvisionDTO orden) {
+        List<SucursalDTO> sucursales = sucursalController.getSucursalesByEstado(true);
+        sucursales.remove(orden.getSucursalDestino());
+        return sucursales.stream()
+                .filter(s -> satisfaceLaOrden(s.getListaProductoCantidadEnStock(), orden.getItemsProductoCantidad()))
+                .collect(Collectors.toList());
     }
 
     private boolean satisfaceLaOrden(Map<ProductoDTO, Integer> stock, Map<ProductoDTO, Integer> order) {
@@ -54,33 +94,9 @@ public class OrdenProvisionManager {
         return order.entrySet().stream().allMatch(isSatisfied);
     }
 
-    private void encontrarCaminos(SucursalDTO actual, SucursalDTO destino,
-            List<RutaDTO> caminoRecorrido, List<RutaDTO> caminoActual, List<List<RutaDTO>> resultado,
-            int limiteTiempoEnMinutos) {
-
-        if (actual.equals(destino)) {
-            Integer duracionTotalMinutos = caminoActual.stream()
-                    .mapToInt(r -> pasarAMinutos(r.getDuracionViaje())).sum();
-            if (duracionTotalMinutos <= limiteTiempoEnMinutos) {
-                resultado.add(new ArrayList<>(caminoActual));
-            }
-            return;
-        }
-
-        List<RutaDTO> rutasDestino = rutaController.getRutasBySucursalDestino(actual);
-        for (RutaDTO ruta : rutasDestino) {
-            if (!caminoRecorrido.contains(ruta)) {
-                caminoActual.add(ruta);
-                caminoRecorrido.add(ruta);
-                encontrarCaminos(ruta.getSucursalDestino(), destino, caminoRecorrido, caminoActual, resultado,
-                        limiteTiempoEnMinutos);
-                caminoActual.remove(ruta);
-                caminoRecorrido.remove(ruta);
-            }
-        }
-    }
-
     private Integer pasarAMinutos(Duration tiempo) {
         return (int) tiempo.toMinutes();
     }
+
+    
 }
