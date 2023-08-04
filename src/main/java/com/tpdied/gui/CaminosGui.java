a51889bfa045e850cfbcdb337cb27744783886d8
Facundo G. Marcolini <fgmarcolini@gmail.com>
2023-08-04 18:44:24 -0300

    Requerimiento Caminos

--- src/main/java/com/tpdied/gui/CaminosGui.java
+++ src/main/java/com/tpdied/gui/CaminosGui.java
@@ -1,7 +1,16 @@
 package com.tpdied.gui;
 
+import com.tpdied.controllers.OrdenProvisionController;
+import com.tpdied.dto.OrdenProvisionDTO;
+import com.tpdied.dto.RutaDTO;
+import com.tpdied.managers.OrdenProvisionManager;
+import com.tpdied.models.EstadoOrden;
+import com.tpdied.util.EntityManagerUtil;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.util.List;
 
 public class CaminosGui implements Tab{
 
@@ -23,73 +32,247 @@ public class CaminosGui implements Tab{
         spCaminos = new javax.swing.JScrollPane(tlCaminos);
         tlCaminos = new javax.swing.JTable();
         btVerCaminos = new javax.swing.JButton();
+        spCaminosCanvas = new javax.swing.JScrollPane(tlCaminos);
+        tlCaminosCanvas = new javax.swing.JTable();
+        btAsignarRecorrido = new javax.swing.JButton();
+        btLimpiarCaminos = new javax.swing.JButton();
+        btVerRuta = new javax.swing.JButton();
 
         jCaminos.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 12)); // NOI18N
         jCaminos.setMaximumSize(new java.awt.Dimension(704, 430));
         jCaminos.setMinimumSize(new java.awt.Dimension(704, 430));
 
         spCaminos.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        tlCaminos.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13)); // NOI18N
-        tlCaminos.setModel(new javax.swing.table.DefaultTableModel(
-                new Object [][] {
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null},
-                        {null, null, null, null}
-                },
-                new String [] {
-                        "Fecha", "S. Destino", "Tiempo Max.", "Productos"
-                }
-        ) {
-             Class[] types = new Class [] {
-                    java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
-            };
-            boolean[] canEdit = new boolean [] {
-                    false, false, false, false
-            };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        tlCaminos.setShowGrid(true);
-        tlCaminos.getTableHeader().setReorderingAllowed(false);
+        getTableOrdenProvision(ordenes);
         spCaminos.setViewportView(tlCaminos);
 
         btVerCaminos.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13)); // NOI18N
         btVerCaminos.setText("Ver Caminos");
+        btVerCaminos.addActionListener(e -> {
+            if(tlCaminos.getSelectedRow() == -1){
+                javax.swing.JOptionPane.showMessageDialog(null, "Debe seleccionar una orden de la tabla para continuar.", "ERROR",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            OrdenProvisionDTO dto = ordenes
+                    .stream()
+                    .filter(o -> o.getId() == (Integer.parseInt(tlCaminos.getValueAt(tlCaminos.getSelectedRow(), 0).toString())))
+                    .findFirst()
+                    .get();
+            caminosPosibles = ordenProvisionManager.getCaminosPosibles(dto);
+            getTableCaminosCanvas(caminosPosibles);
+        });
+
+        spCaminosCanvas.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        getTableCaminosCanvas(null);
+        spCaminosCanvas.setViewportView(tlCaminosCanvas);
+
+        btAsignarRecorrido.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13)); // NOI18N
+        btAsignarRecorrido.setText("Asignar Recorrido");
+        btAsignarRecorrido.addActionListener(e -> {
+            if(tlCaminos.getSelectedRow() == -1){
+                javax.swing.JOptionPane.showMessageDialog(null, "Debe seleccionar una orden de la tabla para continuar.", "ERROR",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            if(tlCaminosCanvas.getSelectedRow() == -1){
+                javax.swing.JOptionPane.showMessageDialog(null, "Debe seleccionar un camino de la tabla para continuar.", "ERROR",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            OrdenProvisionDTO dto = ordenes
+                    .stream()
+                    .filter(o -> o.getId() == (Integer.parseInt(tlCaminos.getValueAt(tlCaminos.getSelectedRow(), 0).toString())))
+                    .findFirst()
+                    .get();
+            dto.setEstado(EstadoOrden.EN_PROCESO);
+            ordenProvisionController.updateOrdenProvision(dto);
+            javax.swing.JOptionPane.showMessageDialog(null, "Orden asignada con éxito", "ÉXITO",
+                    JOptionPane.INFORMATION_MESSAGE);
+            ordenes = ordenProvisionController.getAllOrdenesProvision();
+            getTableOrdenProvision(ordenes);
+        });
+
+        btLimpiarCaminos.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13)); // NOI18N
+        btLimpiarCaminos.setText("Limpiar");
+        btLimpiarCaminos.addActionListener(e -> {
+            tlCaminos.getSelectionModel().clearSelection();
+            tlCaminosCanvas.getSelectionModel().clearSelection();
+            getTableCaminosCanvas(null);
+        });
+
+        btVerRuta.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13)); // NOI18N
+        btVerRuta.setText("Ver Ruta");
+        btVerRuta.addActionListener(e -> {
+            if(tlCaminosCanvas.getSelectedRow() == -1){
+                javax.swing.JOptionPane.showMessageDialog(null, "Debe seleccionar un camino de la tabla para continuar.", "ERROR",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            Integer row = tlCaminosCanvas.getSelectedRow();
+            List<RutaDTO> camino = caminosPosibles
+                    .stream()
+                    .filter(c -> c.get(0).getSucursalOrigen().toString().equals(tlCaminosCanvas.getValueAt(row, 0))
+                    && ordenProvisionManager.getTiempoTotal(c).toString().equals(tlCaminosCanvas.getValueAt(row, 1)))
+                    .findFirst()
+                    .get();
+            new RecorridoGui(camino);
+        });
 
         javax.swing.GroupLayout jCaminosLayout = new javax.swing.GroupLayout(jCaminos);
         jCaminos.setLayout(jCaminosLayout);
         jCaminosLayout.setHorizontalGroup(
                 jCaminosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jCaminosLayout.createSequentialGroup()
-                                .addGap(18, 18, 18)
-                                .addComponent(btVerCaminos)
-                                .addGap(18, 18, 18)
-                                .addComponent(spCaminos, javax.swing.GroupLayout.PREFERRED_SIZE, 309, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(254, Short.MAX_VALUE))
+                                .addContainerGap()
+                                .addGroup(jCaminosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(spCaminos, javax.swing.GroupLayout.PREFERRED_SIZE, 565, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(jCaminosLayout.createSequentialGroup()
+                                                .addComponent(spCaminosCanvas, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addGroup(jCaminosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(btVerCaminos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(btAsignarRecorrido, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(btVerRuta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                        .addComponent(btLimpiarCaminos, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addContainerGap(133, Short.MAX_VALUE))
         );
         jCaminosLayout.setVerticalGroup(
                 jCaminosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jCaminosLayout.createSequentialGroup()
                                 .addContainerGap()
+                                .addComponent(spCaminos, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
                                 .addGroup(jCaminosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(btVerCaminos)
-                                        .addComponent(spCaminos, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(224, Short.MAX_VALUE))
+                                        .addComponent(spCaminosCanvas, javax.swing.GroupLayout.PREFERRED_SIZE, 185, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(jCaminosLayout.createSequentialGroup()
+                                                .addComponent(btVerCaminos)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btVerRuta)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btAsignarRecorrido, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(btLimpiarCaminos)))
+                                .addContainerGap(27, Short.MAX_VALUE))
         );
     }
+
+    private void getTableOrdenProvision(List<OrdenProvisionDTO> listaDTO) {
+        DefaultTableModel modelo = new DefaultTableModel() {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                return String.class;
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+        };
+
+        String[] columnas = {"ID", "Fecha", "S. Destino", "Tiempo Max.", "Productos"};
+        String[] data = new String[columnas.length];
+        for (int i = 0; i < columnas.length; i++) {
+            modelo.addColumn(columnas[i]);
+        }
+
+        if (!(listaDTO == null)) {
+            try {
+                for (OrdenProvisionDTO dto : listaDTO) {
+                    data[0] = dto.getId().toString();
+                    data[1] = dto.getFechaOrden().toString();
+                    data[2] = dto.getSucursalDestino().getNombre();
+                    data[3] = dto.getLimiteTiempo().toString();
+                    data[4] = "";
+                    modelo.addRow(data);
+                }
+
+            } catch (NullPointerException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        tlCaminos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tlCaminos.setModel(modelo);
+        tlCaminos.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13));
+        tlCaminos.setShowGrid(true);
+        tlCaminos.getTableHeader().setReorderingAllowed(false);
+        tlCaminos.getTableHeader().setResizingAllowed(true);
+        tlCaminos.setAutoCreateRowSorter(true);
+        tlCaminos.setFillsViewportHeight(true);
+
+        int i = tlCaminos.getColumnModel().getColumnCount() - 1;
+
+        while (i >= 0) {
+            tlCaminos.getColumnModel().getColumn(i).setResizable(true);
+            i--;
+        }
+    }
+
+    private void getTableCaminosCanvas(List<List<RutaDTO>> caminos) {
+        DefaultTableModel modelo = new DefaultTableModel() {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                return String.class;
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return false;
+            }
+        };
+
+        String[] columnas = {"S. Origen", "Tiempo"};
+        String[] data = new String[columnas.length];
+        for (int i = 0; i < columnas.length; i++) {
+            modelo.addColumn(columnas[i]);
+        }
+
+        if (!(caminos == null)) {
+            try {
+                for (List<RutaDTO> dto : caminos) {
+                    data[0] = dto.get(0).getSucursalOrigen().toString();
+                    data[1] = ordenProvisionManager.getTiempoTotal(dto);
+                    modelo.addRow(data);
+                }
+
+            } catch (NullPointerException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        tlCaminosCanvas.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        tlCaminosCanvas.setModel(modelo);
+        tlCaminosCanvas.setFont(new java.awt.Font("Noto Sans", Font.PLAIN, 13));
+        tlCaminosCanvas.setShowGrid(true);
+        tlCaminosCanvas.getTableHeader().setReorderingAllowed(false);
+        tlCaminosCanvas.getTableHeader().setResizingAllowed(true);
+        tlCaminosCanvas.setAutoCreateRowSorter(true);
+        tlCaminosCanvas.setFillsViewportHeight(true);
+
+        int i = tlCaminosCanvas.getColumnModel().getColumnCount() - 1;
+
+        while (i >= 0) {
+            tlCaminosCanvas.getColumnModel().getColumn(i).setResizable(true);
+            i--;
+        }
+    }
+
 
     private javax.swing.JButton btVerCaminos;
     private javax.swing.JPanel jCaminos;
     private javax.swing.JScrollPane spCaminos;
     private javax.swing.JTable tlCaminos;
+    private javax.swing.JScrollPane spCaminosCanvas;
+    private javax.swing.JTable tlCaminosCanvas;
+    private javax.swing.JButton btAsignarRecorrido;
+    private javax.swing.JButton btLimpiarCaminos;
+    private javax.swing.JButton btVerRuta;
 
-
+    private final OrdenProvisionController ordenProvisionController = new OrdenProvisionController(EntityManagerUtil.getEntityManager());
+    private final OrdenProvisionManager ordenProvisionManager = new OrdenProvisionManager(EntityManagerUtil.getEntityManager());
+    private List<OrdenProvisionDTO> ordenes = ordenProvisionController.getOrdenesProvisionPendientes();
+    private List<List<RutaDTO>> caminosPosibles;
 }
+
---
+++ src/main/java/com/tpdied/gui/RecorridoGui.java
@@ -0,0 +1,195 @@
+package com.tpdied.gui;
+
+import com.tpdied.dto.RutaDTO;
+import com.tpdied.managers.OrdenProvisionManager;
+import com.tpdied.util.EntityManagerUtil;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RecorridoGui {
+    public RecorridoGui(List<RutaDTO> camino){
+        JFrame ventana = new JFrame("Recorrido de camino");
+        ventana.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        ventana.setSize(500,500);
+
+        GrafoCanvas canvas = new GrafoCanvas(camino);
+        ventana.getContentPane().add(canvas);
+        ventana.setLocationRelativeTo(null);
+
+        ventana.setVisible(true);
+    }
+}
+class Node {
+    public int x, y, weight, height;
+    public String name;
+
+    public Node(int x, int y, int weight, int height, String name) {
+        this.x = x;
+        this.y = y;
+        this.weight = weight;
+        this.height = height;
+        this.name = name;
+    }
+
+    public Ellipse2D.Float getShape() {
+        return new Ellipse2D.Float(this.x, this.y, this.weight, this.height);
+    }
+}
+class Arista {
+    public int x1, y1, x2, y2;
+
+    public Arista(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public Line2D.Float getShape() {
+        return new Line2D.Float(this.x1, this.y1, this.x2, this.y2);
+    }
+}
+
+class GrafoCanvas extends Canvas {
+    private List<RutaDTO> camino;
+    private OrdenProvisionManager ordenProvisionManager = new OrdenProvisionManager(EntityManagerUtil.getEntityManager());
+    private List<String> sucursales;
+
+    public GrafoCanvas (List<RutaDTO> camino){
+        super();
+        this.camino = camino;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        sucursales = ordenProvisionManager
+                .getSucursalesDeCamino(camino)
+                .stream()
+                .map(s -> s.getNombre().toLowerCase())
+                .collect(Collectors.toList());
+
+        // Creo los nodos
+        Node nodoPuerto = new Node(30, 200, 30, 30, "Puerto");
+
+        // Norte
+        Node nodoB = new Node(110, 110, 30, 30, "B");
+        Node nodoD = new Node(190, 110, 30, 30, "D");
+        Node nodoE = new Node(270, 110, 30, 30, "E");
+
+        // Centro
+        Node nodoC = new Node(110, 200, 30, 30, "C");
+        Node nodoF = new Node(190, 200, 30, 30, "F");
+        Node nodoG = new Node(270, 200, 30, 30, "G");
+        Node nodoCentro = new Node(360, 200, 30, 30, "Casa Central");
+
+        // Sur
+        Node nodoX = new Node(110, 290, 30, 30, "X");
+        Node nodoY = new Node(190, 290, 30, 30, "Y");
+        Node nodoZ = new Node(270, 290, 30, 30, "Z");
+
+        // Aristas
+        Arista aristaPuertoB = new Arista(getMedioX(nodoPuerto), getMedioY(nodoPuerto), getMedioX(nodoB), getMedioY(nodoB));
+        Arista aristaPuertoC = new Arista(getMedioX(nodoPuerto), getMedioY(nodoPuerto), getMedioX(nodoC), getMedioY(nodoC));
+        Arista aristaPuertoX = new Arista(getMedioX(nodoPuerto), getMedioY(nodoPuerto), getMedioX(nodoX), getMedioY(nodoX));
+
+        Arista aristaBD = new Arista(getMedioX(nodoB), getMedioY(nodoB), getMedioX(nodoD), getMedioY(nodoD));
+        Arista aristaBC = new Arista(getMedioX(nodoB), getMedioY(nodoB), getMedioX(nodoC), getMedioY(nodoC));
+
+        Arista aristaCD = new Arista(getMedioX(nodoC), getMedioY(nodoC), getMedioX(nodoD), getMedioY(nodoD));
+        Arista aristaCF = new Arista(getMedioX(nodoC), getMedioY(nodoC), getMedioX(nodoF), getMedioY(nodoF));
+
+        Arista aristaXC = new Arista(getMedioX(nodoX), getMedioY(nodoX), getMedioX(nodoC), getMedioY(nodoC));
+        Arista aristaXG = new Arista(getMedioX(nodoX), getMedioY(nodoX), getMedioX(nodoG), getMedioY(nodoG));
+        Arista aristaXY = new Arista(getMedioX(nodoX), getMedioY(nodoX), getMedioX(nodoY), getMedioY(nodoY));
+
+        Arista aristaDE = new Arista(getMedioX(nodoD), getMedioY(nodoD), getMedioX(nodoE), getMedioY(nodoE));
+        Arista aristaDF = new Arista(getMedioX(nodoD), getMedioY(nodoD), getMedioX(nodoF), getMedioY(nodoF));
+
+        Arista aristaFG = new Arista(getMedioX(nodoF), getMedioY(nodoF), getMedioX(nodoG), getMedioY(nodoG));
+        Arista aristaFY = new Arista(getMedioX(nodoF), getMedioY(nodoF), getMedioX(nodoY), getMedioY(nodoY));
+
+        Arista aristaYZ = new Arista(getMedioX(nodoY), getMedioY(nodoY), getMedioX(nodoZ), getMedioY(nodoZ));
+
+        Arista aristaECentro = new Arista(getMedioX(nodoE), getMedioY(nodoE), getMedioX(nodoCentro), getMedioY(nodoCentro));
+
+        Arista aristaGE = new Arista(getMedioX(nodoG), getMedioY(nodoG), getMedioX(nodoE), getMedioY(nodoE));
+        Arista aristaGCentro = new Arista(getMedioX(nodoG), getMedioY(nodoG), getMedioX(nodoCentro), getMedioY(nodoCentro));
+
+        Arista aristaZCentro = new Arista(getMedioX(nodoZ), getMedioY(nodoZ), getMedioX(nodoCentro), getMedioY(nodoCentro));
+
+
+        // Dibujo las aristas
+        g2.setColor(Color.BLACK);
+        g2.draw(aristaPuertoB.getShape());
+        g2.draw(aristaPuertoC.getShape());
+        g2.draw(aristaPuertoX.getShape());
+        g2.draw(aristaBD.getShape());
+        g2.draw(aristaBC.getShape());
+        g2.draw(aristaCD.getShape());
+        g2.draw(aristaCF.getShape());
+        g2.draw(aristaXC.getShape());
+        g2.draw(aristaXG.getShape());
+        g2.draw(aristaXY.getShape());
+        g2.draw(aristaDE.getShape());
+        g2.draw(aristaDF.getShape());
+        g2.draw(aristaFG.getShape());
+        g2.draw(aristaFY.getShape());
+        g2.draw(aristaYZ.getShape());
+        g2.draw(aristaECentro.getShape());
+        g2.draw(aristaGE.getShape());
+        g2.draw(aristaGCentro.getShape());
+        g2.draw(aristaZCentro.getShape());
+
+        // Dibujo los nodos
+        draw(nodoPuerto, g2);
+        draw(nodoB, g2);
+        draw(nodoD, g2);
+        draw(nodoE, g2);
+        draw(nodoC, g2);
+        draw(nodoF, g2);
+        draw(nodoG, g2);
+        draw(nodoCentro, g2);
+        draw(nodoX, g2);
+        draw(nodoY, g2);
+        draw(nodoZ, g2);
+
+        g2.setColor((Color.BLACK));
+        g2.drawString(nodoPuerto.name, nodoPuerto.x, nodoPuerto.y);
+        g2.drawString(nodoB.name, nodoB.x, nodoB.y);
+        g2.drawString(nodoC.name, nodoC.x, nodoC.y);
+        g2.drawString(nodoD.name, nodoD.x, nodoD.y);
+        g2.drawString(nodoE.name, nodoE.x, nodoE.y);
+        g2.drawString(nodoF.name, nodoF.x, nodoF.y);
+        g2.drawString(nodoG.name, nodoG.x, nodoG.y);
+        g2.drawString(nodoX.name, nodoX.x, nodoX.y);
+        g2.drawString(nodoY.name, nodoY.x, nodoY.y);
+        g2.drawString(nodoZ.name, nodoZ.x, nodoZ.y);
+        g2.drawString(nodoCentro.name, nodoCentro.x, nodoCentro.y);
+    }
+
+    private void draw(Node nodo, Graphics2D g2) {
+        if (sucursales.contains(nodo.name.toLowerCase())){
+            g2.setColor((Color.GREEN));
+            g2.fill(nodo.getShape());
+        }
+        else{
+            g2.setColor(Color.BLUE);
+            g2.fill(nodo.getShape());
+        }
+    }
+
+    public Integer getMedioX(Node n) {
+        return n.x + (n.weight / 2);
+    }
+    public Integer getMedioY(Node n) {
+        return n.y + (n.height / 2);
+    }
+}
